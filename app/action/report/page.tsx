7baf153554fdd8aba0d7150736f5646b5292b5f2
Jimmy Bergsma <jimmybergsma3@gmail.com>
2026-01-18 18:36:28 +0100

    Fix empty report action page

---
+++ app/action/report/page.tsx
@@ -0,0 +1,11 @@
+export default function ReportActionPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Report</h1>
+      <p>This page is under construction.</p>
+      <p>
+        Use the public passport route for now: <code>/passport/ER-REG-001</code>
+      </p>
+    </div>
+  );
+}
